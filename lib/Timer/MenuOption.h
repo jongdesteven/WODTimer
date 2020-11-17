ec32f730cc3a30654746ac9f4e33043ea15ce06e
Steven de Jong <jongdesteven@gmail.com>
2020-11-17 10:55:04 +0100

    moved first file to separate header file /2

--- lib/Timer/MenuOption.h
+++ lib/Timer/MenuOption.h
@@ -2,6 +2,7 @@
 #define MENUOPTION_H
 
 class MenuOption {
+private:
   const char* displayName; //2 green characters
   int startTimeInterval1Sec;
   int startTimeInterval2Sec;
@@ -9,60 +10,15 @@ class MenuOption {
   bool countDirectionUp;
   bool includesInterval;
 
-  public:
-    MenuOption(const char* name, int time1, int time2, int rounds, bool countUp, bool interval) :
-      displayName(name),
-      startTimeInterval1Sec(time1),
-      startTimeInterval2Sec(time2),
-      nrOfRounds(rounds),
-      countDirectionUp(countUp),
-      includesInterval(interval)
-    {
-    }
-
-    const char* getDisplayName(){
-      return displayName;
-    }
-    
-    int getStartTime1(){
-      return startTimeInterval1Sec;
-    }
-    
-    int getStartTime2(){
-      return startTimeInterval2Sec;
-    }
-    
-    int getNrOfRounds(){
-      return nrOfRounds;
-    }
-    
-    bool getCountDirectionUp(){
-      return countDirectionUp;
-    }
-  
-    void changeRounds(int change){
-      // do not add rounds if it never had
-      if (nrOfRounds > 0){
-        nrOfRounds += change;
-        if (nrOfRounds < 1) { nrOfRounds = 1;}
-        if (nrOfRounds > 99) { nrOfRounds = 99; }
-      }
-    }
-   
-    void changeTimeInterval(int interval, int changeSec){
-      switch (interval){
-      case 1:
-        startTimeInterval1Sec += changeSec;
-        if (startTimeInterval1Sec < 1){ startTimeInterval1Sec = 1; }
-        if (startTimeInterval1Sec > 3600){ startTimeInterval1Sec = 3600; }
-        break;
-      case 2:
-        startTimeInterval2Sec += changeSec;
-        if (startTimeInterval2Sec < 0){ startTimeInterval2Sec = 0; }
-        if (startTimeInterval2Sec > 3600){ startTimeInterval2Sec = 3600; }
-        break; 
-      }
-    }
+public:
+  MenuOption(const char* name, int time1, int time2, int rounds, bool countUp, bool interval);
+  const char* getDisplayName();
+  int getStartTime1();
+  int getStartTime2();
+  int getNrOfRounds();
+  bool getCountDirectionUp();
+  void changeRounds(int change);
+  void changeTimeInterval(int interval, int changeSec);
 };
 
 #endif
